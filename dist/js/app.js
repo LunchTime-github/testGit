87720aa4210bb9161331087d0dfca6ed0fa03141
LunchTime-github <hiyoon9@gmail.com>
2019-10-17 16:32:58 +0900

    2.0 Canvas Events

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,29 @@
 "use strict";
 
-var ii = " ";
+var canvas = document.getElementById("jsCanvas");
 
-var app = "";
+var painting = false;
+
+function onMouseMove(event) {
+  var x = event.offsetX;
+  var y = event.offsetY;
+}
+
+function onMouseDown(event) {
+  painting = true;
+}
+
+function onMouseUp(event) {
+  painting = false;
+}
+
+function onMouseLeave(event) {
+  painting = false;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", onMouseLeave);
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
-const ii = " ";
+const canvas = document.getElementById("jsCanvas");
 
-const app = "";
+let painting = false;
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+}
+
+function onMouseDown(event) {
+  painting = true;
+}
+
+function onMouseUp(event) {
+  painting = false;
+}
+
+function onMouseLeave(event) {
+  painting = false;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", onMouseLeave);
+}
